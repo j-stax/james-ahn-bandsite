f1fba949a9c700c5231e1e7cac5398b3c46b31a8
j-stax <ahnjames01@gmail.com>
2024-12-01 20:45:29 -0500

    Comments form feature - submit new comment and append it to the existing list; display new list on page.

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,41 +1,120 @@
 const data = [
     {
-        author: "Victor Pinto",
+        user: "Victor Pinto",
         timestamp: "11/02/2023",
         text: "This is art. This is inexplicable magic expressed in the purest way everything that makes " + 
                 "up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
     },
     {
-        author: "Christina Cabrera",
+        user: "Christina Cabrera",
         timestamp: "10/28/2023",
         text: "I feel blessed to have seen them in person. What a show! They were just perfection. " + 
                 "If there was one day of my life I could relive, this would be it. What an incredible day."
     },
     {
-        author: "Issac Tadesse",
+        user: "Issac Tadesse",
         timestamp: "10/20/2023",
         text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. " + 
                 "Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
     }
 ];
 
-window.addEventListener("DOMContentLoaded", loadComments);
+const dateMonthMap = {
+    0: "01",
+    1: "02",
+    2: "03",
+    3: "04",
+    4: "05",
+    5: "06",
+    6: "07",
+    7: "08",
+    8: "09",
+    9: "10",
+    10: "11",
+    11: "12"
+};
+
+window.addEventListener("DOMContentLoaded", loadedHandler);
+
+// Initialize dynamic page features
+function loadedHandler() {
+    loadComments();
+    const commentForm = document.querySelector(".comments__form");
+    commentForm.addEventListener("submit", submitHandler);
+}
 
 function loadComments() {
-    for (let i = 0; i < data.length; i++) {
-        displayComment(data[i]);
+    for (let commentObj of data) {
+        createNewCommentComponent(commentObj);
+    }
+}
+
+/* Comment form handler.
+ * Validates inputs and appends new comment component to the page.
+*/
+function submitHandler(event) {
+    event.preventDefault();
+    const form = event.target;
+    const userVal = form.user.value.trim();
+    const commentVal = form.comment.value.trim();
+    
+    if (!isValid(userVal)) {
+        document.getElementById("user").style.borderColor = "#D22D2D";
+    }
+
+    if (!isValid(commentVal)) {
+        document.getElementById("comment").style.borderColor = "#D22D2D";
+    }
+
+    if (isValid(userVal) && isValid(commentVal)) {
+        document.getElementById("user").style.borderColor = "#E1E1E1";
+        document.getElementById("comment").style.borderColor = "#E1E1E1";
+
+        let date = new Date();
+        date = `${dateMonthMap[date.getMonth()]}/${date.getDate()}/${date.getFullYear()}`;
+
+        const newCommentObj = {
+            user: userVal,
+            timestamp: date,
+            text: commentVal
+        };
+
+        data.push(newCommentObj);
+        resetCommentsContainer();
+        loadComments();
+        form.reset();
+    }
+}
+
+// Form validation
+function isValid(inputField) {
+    if (inputField.length == 0 || inputField == null) {
+        return false;
+    }
+    return true;
+}
+
+
+// Clear all comments from the page
+function resetCommentsContainer() {
+    let commentsContainer = document.querySelector(".comments__body-container");
+    const childNodes = commentsContainer.children;
+
+    for (let i = childNodes.length-1; i > 1; i--) {
+        let childNode = commentsContainer.children[i];
+        childNode.parentNode.removeChild(childNode);
     }
 }
 
 // Create a new comment component and append to the DOM tree
-function displayComment(commentObject) {
+function createNewCommentComponent(commentObject) {
     // Create component elements
     const parentNode = document.querySelector(".comments__body-container");
     const newComponentNode = document.createElement("article");
     const newAvatarNode = document.createElement("div");
-    const newDivNode = document.createElement("div");
+    const newCommentTextContainerNode = document.createElement("div");
     const newCommentHeaderNode = document.createElement("div"); 
-    const newCommentAuthorNode = document.createElement("p");
+    const newCommentUserNode = document.createElement("p");
     const newCommentTimestampNode = document.createElement("p");
     const newCommentNode = document.createElement("p");
     const newDividerNode = document.createElement("hr");
@@ -43,28 +122,29 @@ function displayComment(commentObject) {
     // Add class names for styling
     newComponentNode.classList.add("comments__comment-component");
     newAvatarNode.classList.add("comments__avatar-placeholder");
+    newCommentTextContainerNode.classList.add("comments__text-container");
     newCommentHeaderNode.classList.add("comments__component-header");
-    newCommentAuthorNode.classList.add("comments__author");
+    newCommentUserNode.classList.add("comments__user");
     newCommentTimestampNode.classList.add("comments__timestamp");
     newCommentNode.classList.add("comments__text");
 
     // Create text nodes for comment object values
-    const newAuthorTextNode = document.createTextNode(commentObject.author);
+    const newUserTextNode = document.createTextNode(commentObject.user);
     const newTimestampTextNode = document.createTextNode(commentObject.timestamp);
     const newCommentTextNode = document.createTextNode(commentObject.text);
 
     // Append the text nodes to their respective element nodes
-    newCommentAuthorNode.appendChild(newAuthorTextNode);
+    newCommentUserNode.appendChild(newUserTextNode);
     newCommentTimestampNode.appendChild(newTimestampTextNode);
     newCommentNode.appendChild(newCommentTextNode);
 
     // Create the DOM tree for the comment component
-    newCommentHeaderNode.appendChild(newCommentAuthorNode);
+    newCommentHeaderNode.appendChild(newCommentUserNode);
     newCommentHeaderNode.appendChild(newCommentTimestampNode);
-    newDivNode.appendChild(newCommentHeaderNode);
-    newDivNode.appendChild(newCommentTextNode);
+    newCommentTextContainerNode.appendChild(newCommentHeaderNode);
+    newCommentTextContainerNode.appendChild(newCommentTextNode);
     newComponentNode.appendChild(newAvatarNode);
-    newComponentNode.appendChild(newDivNode);
+    newComponentNode.appendChild(newCommentTextContainerNode);
 
     // Append the comment component to the comments section in the DOM tree
     parentNode.appendChild(newComponentNode);
